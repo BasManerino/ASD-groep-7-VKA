1296537f2ab1ebfda761c2bdfd80445f3a249e6f
Jesse <jessepols@gmail.com>
2020-10-03 14:30:55 +0200

    initial command function

--- src/main/java/nl/rls/ASD/vehicle/port/service/TrainService.java
+++ src/main/java/nl/rls/ASD/vehicle/port/service/TrainService.java
@@ -1,5 +1,16 @@
 package nl.rls.ASD.vehicle.port.service;
 
 public class TrainService {
+    public boolean addNewTrain(String type, int nr, String timeAtHannover, String timeAtTransfer, String tPoint, int compId, String jSections, String mStatuses, String cMessages) {
 
+
+
+
+
+
+
+
+
+        return true;
+    }
 }
