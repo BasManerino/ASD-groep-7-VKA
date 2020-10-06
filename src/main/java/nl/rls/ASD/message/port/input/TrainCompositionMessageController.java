3685c828ff0f2ab59b30195e3b3c6d3d85c24f46
Sebastiaan Jansen <33654457+BasManerino@users.noreply.github.com>
2020-10-06 14:50:59 +0200

    Update TrainCompositionMessageController.java

--- src/main/java/nl/rls/ASD/message/port/input/TrainCompositionMessageController.java
+++ src/main/java/nl/rls/ASD/message/port/input/TrainCompositionMessageController.java
@@ -5,6 +5,7 @@ import java.util.Date;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -21,15 +22,19 @@ import nl.rls.ci.url.BaseURL;
 @RequestMapping(BaseURL.BASE_PATH + "/traincompositionmessage")
 public class TrainCompositionMessageController implements TrainCompositionMessageInputAdapter{
 
+	@ExceptionHandler
     @PostMapping(value = "/create", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
-    public boolean createTrainCompositionMessage(String objectType, CompanyId company, String core, String variant, int timetableYear, Date startDate,
-    		TrainId train, MessageStatus messageStatus, int messageType, String messageTypeVersion, String messageIdentifier, Date messageDateTime,
-    		String senderReference, CompanyId sender, CompanyId recipient, boolean companyCheck, boolean trainCheck, boolean senderCheck, boolean recipientCheck) throws SQLException
-    {
+    public boolean createTrainCompositionMessage(String objectType, Integer company, String core, String variant, int timetableYear, Date startDate,
+    		Integer train, MessageStatus messageStatus, int messageType, String messageTypeVersion, String messageIdentifier, Date messageDateTime,
+    		String senderReference, Integer sender, Integer recipient, boolean companyCheck, boolean trainCheck, boolean senderCheck, boolean recipientCheck) {
     	TrainCompositionMessageApplicationService service = new TrainCompositionMessageApplicationService();
-    	return service.createTrainCompositionMessage(objectType, company, core, variant, timetableYear, startDate,
-        		train, messageStatus, messageType, messageTypeVersion, messageIdentifier, messageDateTime,
-        		senderReference, sender, recipient, companyCheck, trainCheck, senderCheck, recipientCheck);
+    	CompanyId company2 = new CompanyId(company);
+    	TrainId train2 = new TrainId(train);
+    	CompanyId sender2 = new CompanyId(sender);
+    	CompanyId recipient2 = new CompanyId(recipient);
+    	return service.createTrainCompositionMessage(objectType, company2, core, variant, timetableYear, startDate,
+        		train2, messageStatus, messageType, messageTypeVersion, messageIdentifier, messageDateTime,
+        		senderReference, sender2, recipient2, companyCheck, trainCheck, senderCheck, recipientCheck);
     }
 }
